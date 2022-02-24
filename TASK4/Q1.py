5c2c7f4845cbc0bc1b230f15384c3fcb31b3af30
lavanya1805 <99705278+lavanya1805@users.noreply.github.com>
2022-02-24 07:41:06 +0530

    Add files via upload

---
+++ TASK4/Q1.py
@@ -0,0 +1,6 @@
+# Write a program to perform addition on two user input numbers.
+
+first_number = float(input("first_number = "))
+second_number = float(input("second_number = "))
+sum = (first_number) + (second_number)
+print ("sum = " + str(sum))
