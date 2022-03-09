f0adf98146b653ae59d8fbd7e1c91ea92261fc75
lavanya1805 <99705278+lavanya1805@users.noreply.github.com>
2022-03-09 20:48:11 +0530

    Add files via upload

---
+++ TASK 8/Ques 4.py
@@ -0,0 +1,10 @@
+import pandas as pd
+series = pd.Series(['amrita', 'school', 'of', 'engineering', 'chennai', 'campus'])
+new_series =series.str.capitalize()
+print(series)
+print(new_series)
+
+
+
+
+
---
+++ TASK 8/Ques 5(1).py
@@ -0,0 +1,13 @@
+#question 5(1)
+#addition of 2 numpy arrays
+import numpy as np
+arr1 = np.array([1, 2, 3])
+arr2 = np.array([3, 4, 5])
+add_arr = np.add(arr1, arr2)
+print("1st array :", arr1)
+print("2nd array :", arr2)
+print("sum:", add_arr)
+
+
+
+
