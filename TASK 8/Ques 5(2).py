819fc6d339439f69a379884be9749cecc7f0b92f
lavanya1805 <99705278+lavanya1805@users.noreply.github.com>
2022-03-09 21:00:05 +0530

    Add files via upload

---
+++ TASK 8/Ques 5(2).py
@@ -0,0 +1,11 @@
+# Question 5(2)
+# multiplying a matrix
+import numpy as np
+s1 = np.array([[1,4,7],[2,5,8],[4,6,8]])
+s2 = np.array([[1,4,3],[2,5,4],[3,6,5]])
+s3 = np.dot(s1,s2)
+print(s3)
+
+
+
+
